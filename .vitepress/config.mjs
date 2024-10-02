d09dd61e5177e16a9598f7c15e729c31fb730997
rxLiu <TTDSY0425@163.com>
2024-10-02 21:05:27 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,167 @@
+import { defineConfig } from 'vitepress'
+import mathjax3 from 'markdown-it-mathjax3';
+
+const customElements = [
+	'mjx-container',
+    'mjx-assistive-mml',
+	'math',
+	'maction',
+	'maligngroup',
+	'malignmark',
+	'menclose',
+	'merror',
+	'mfenced',
+	'mfrac',
+	'mi',
+	'mlongdiv',
+	'mmultiscripts',
+	'mn',
+	'mo',
+	'mover',
+	'mpadded',
+	'mphantom',
+	'mroot',
+	'mrow',
+	'ms',
+	'mscarries',
+	'mscarry',
+	'mscarries',
+	'msgroup',
+	'mstack',
+	'mlongdiv',
+	'msline',
+	'mstack',
+	'mspace',
+	'msqrt',
+	'msrow',
+	'mstack',
+	'mstack',
+	'mstyle',
+	'msub',
+	'msup',
+	'msubsup',
+	'mtable',
+	'mtd',
+	'mtext',
+	'mtr',
+	'munder',
+	'munderover',
+	'semantics',
+	'math',
+	'mi',
+	'mn',
+	'mo',
+	'ms',
+	'mspace',
+	'mtext',
+	'menclose',
+	'merror',
+	'mfenced',
+	'mfrac',
+	'mpadded',
+	'mphantom',
+	'mroot',
+	'mrow',
+	'msqrt',
+	'mstyle',
+	'mmultiscripts',
+	'mover',
+	'mprescripts',
+	'msub',
+	'msubsup',
+	'msup',
+	'munder',
+	'munderover',
+	'none',
+	'maligngroup',
+	'malignmark',
+	'mtable',
+	'mtd',
+	'mtr',
+	'mlongdiv',
+	'mscarries',
+	'mscarry',
+	'msgroup',
+	'msline',
+	'msrow',
+	'mstack',
+	'maction',
+	'semantics',
+	'annotation',
+	'annotation-xml',
+];
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  
+  head: [["link", { rel: "icon", href: "/public/logo.png" }]],
+  title: "Louis的笔记本",
+  description: "A VitePress Site",
+  themeConfig: {
+    outlineTitle:"目录",
+    outline:[2,6],
+    logo: '/public/logo.png',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '科研', items:[
+        {text:'机器学习',link: '/机器学习.md'},
+        {text:'动手学深度学习',link: '/动手学深度学习.md'},
+        {text:'动手学强化学习',link: '/动手学强化学习.md'},
+        {text:'运筹学',link: '/运筹学.md'},]},
+      { text: '工作', items:[
+          {text:'算法',link: '/算法.md'}]},
+      {text:'随笔',link:'/'}
+    ],
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+    sidebar: false, // 关闭侧边栏
+    aside: "left", // 设置右侧侧边栏在左侧显示
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/RuiXuan-Liu' }
+    ],
+    footer: {
+      copyright: "LRX版权所有 @ 2024-2054 请尊重他人劳动成果，未经允许禁止转载"
+    },
+       // 设置搜索框的样式
+       search: {
+        provider: "local",
+        options: {
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              noResultsText: "无法找到相关结果",
+              resetButtonTitle: "清除查询条件",
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+              },
+            },
+          },
+        },
+      },
+  },
+  markdown: {
+    config: (md) => {
+      md.use(mathjax3);
+    },
+  },
+  vue: {
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => customElements.includes(tag),
+      },
+    },
+  },
+  base: "/personal-blog/"
+})
+
+
---
+++ index.md
@@ -0,0 +1,26 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Louis的笔记本"
+  text: "记录当下的每一刻"
+  tagline: Carpe diem, mea amicus — seize the day, my friend
+  image:
+    src: public\background.png
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: Start exploring here
+      link: /markdown-examples
+
+
+features:
+  - title: 自省
+    details: 莫见长安行乐处，空令岁月易蹉跎
+  - title: 命运
+    details: 时来天地皆同力，运去英雄不自由
+  - title: 岁月
+    details: 桃李春风一杯酒，江湖夜雨十年灯
+---
+
